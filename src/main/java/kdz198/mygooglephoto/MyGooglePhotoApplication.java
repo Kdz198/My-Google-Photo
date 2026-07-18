e3ffdd951f340c8daf26b0be7f588fac4e061987
HoangTuGio <khangdeptrai198@gmail.com>
2026-07-19 00:07:21 +0700

    initialize My-Google-Photo project with Maven and Spring Boot setup

---
+++ src/main/java/kdz198/mygooglephoto/MyGooglePhotoApplication.java
@@ -0,0 +1,13 @@
+package kdz198.mygooglephoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGooglePhotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGooglePhotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kdz198/mygooglephoto/MyGooglePhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package kdz198.mygooglephoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGooglePhotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
