d2f2e9c1712cccfad5e94d9135e0b1029320e3a3
HoangTuGio <khangdeptrai198@gmail.com>
2026-07-18 17:20:47 +0000

    style: auto format code with spotless [skip ci]

--- src/main/java/kdz198/mygooglephoto/MyGooglePhotoApplication.java
+++ src/main/java/kdz198/mygooglephoto/MyGooglePhotoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyGooglePhotoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MyGooglePhotoApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(MyGooglePhotoApplication.class, args);
+  }
 }
